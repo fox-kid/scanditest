7088f8577f8c366306e643c958304f40c4d5c85a
fox-kid <alezh15@freeuni.edu.ge>
2022-02-09 22:04:59 +0400

    test copmmit

---
+++ scanditest/src/index.js
@@ -0,0 +1,41 @@
+<div class="CheckoutProgressBar_progress_bar_wrapper__30p1F">
+  <div class="CheckoutProgressBar_steps__2N6Xx">
+    <div class="CheckoutProgressBar_step__line__Omypi CheckoutProgressBar_step__line_first__tAb-K">
+      <div></div>
+    </div>
+    <div class="CheckoutProgressBar_step__item_list__Ihl1U">
+      <div class="CheckoutProgressBar_step_item__3fKkx">
+        <div class="CheckoutProgressBar_step__line__Omypi">
+          <div></div>
+        </div>
+        <div class="CheckoutProgressBar_step_item__circle__3qMlU">
+          <span class="CheckoutProgressBar_step_item__number__2EH9T">
+            <svg viewBox="0 0 490 490">
+              <path
+                fill="currentColor"
+                d="M452.253 28.326 197.831 394.674 29.044 256.875 0 292.469l207.253 169.205L490 54.528z"
+              ></path>
+            </svg>
+          </span>
+          <span class="CheckoutProgressBar_step_item__title__BUnZn">
+            shipping
+          </span>
+        </div>
+      </div>
+      <div class="CheckoutProgressBar_step_item__3fKkx CheckoutProgressBar_is_active__wJSFt">
+        <div class="CheckoutProgressBar_step__line__Omypi">
+          <div></div>
+        </div>
+        <div class="CheckoutProgressBar_step_item__circle__3qMlU">
+          <span class="CheckoutProgressBar_step_item__number__2EH9T">2</span>
+          <span class="CheckoutProgressBar_step_item__title__BUnZn">
+            billing
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="CheckoutProgressBar_step__line__Omypi CheckoutProgressBar_step__line_last__2UxL1">
+      <div></div>
+    </div>
+  </div>
+</div>;
